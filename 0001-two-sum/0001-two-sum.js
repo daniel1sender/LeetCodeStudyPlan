9d3291fbac8f9760550b6c264765ae5d7057fdb4
Daniel Sender <70211437+daniel1sender@users.noreply.github.com>
2024-08-16 14:00:00 +0300

    Time: 68 ms (52.61%), Space: 50.4 MB (54.59%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,19 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    let sum = 0;
+    const pairIndex = {};
+
     for (let i = 0; i < nums.length; i++) {
-        for (let j = 1; j < nums.length; j++) {
-            //console.log(`i: ${i}`);
-            //console.log(`j: ${j}`);
-            //console.log(nums[i]);
-            //console.log(nums[j]);
-            if (i !== j) {
-                sum = nums[i] + nums[j];
-                if (sum === target) {
-                    return [i, j];
-                }
-            }
+        const currentNum = nums[i];
+
+        if (target - currentNum in pairIndex) {
+            return [i, pairIndex[target - currentNum]];
         }
+        pairIndex[currentNum] = i;
     }
 };
