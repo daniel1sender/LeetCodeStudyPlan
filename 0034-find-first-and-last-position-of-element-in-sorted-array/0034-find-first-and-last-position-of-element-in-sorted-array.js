8e765378b346b5e26b767fcd2f01d5908c20c588
Daniel Sender <70211437+daniel1sender@users.noreply.github.com>
2024-12-08 14:36:27 +0200

    Time: 1 ms (21.68%), Space: 50.4 MB (12.08%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -16,26 +16,21 @@ var searchRange = function (nums, target) {
         let mid = Math.floor((left + right) / 2);
 
         if (nums[mid] === target) {
+
             nums.forEach((value, index) => {
                 if (value === target) arrayIndex.push(index);
             });
-
-            arrayIndex.forEach((value, index) => {
-                if (index === 0 || index === arrayIndex.length - 1) {
-                    newArrayIndex.push(value);
-                }
-            });
-
-            if (newArrayIndex.length === 1) {
-                newArrayIndex.push(newArrayIndex[0]);
-            }
+            let firstValue = arrayIndex.find((_, index) => index === 0)
+            let lastValue = arrayIndex.find((_, index) => index === arrayIndex.length - 1);
+            newArrayIndex.push(firstValue, lastValue);
             return newArrayIndex;
+
         } else if (nums[mid] < target) {
             left = mid + 1;
         } else {
             right = mid - 1;
         }
     }
+    
     return [-1, -1];
-
 };
